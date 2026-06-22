cd0c614ed434abeb171edfd0ac2309194d404767
hyein0514 <hi020514@gmail.com>
2026-06-22 14:18:13 +0900

    refactor: 엔티티 코드 수정

--- src/main/java/com/aims/assembly/domain/enums/EquipmentOperationStatus.java
+++ src/main/java/com/aims/assembly/domain/enums/EquipmentOperationStatus.java
@@ -1,7 +1,6 @@
 package com.aims.assembly.domain.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
 @RequiredArgsConstructor
@@ -9,8 +8,9 @@ public enum EquipmentOperationStatus {
 
     RUNNING("가동 중", true),
     IDLE("대기 중", false),
+    WARNING("경고", true),
     STOPPED("정지", false),
-    ERROR("오류", false),
+    FAULT("고장", false),
     MAINTENANCE("점검 중", false);
 
     private final String description;
